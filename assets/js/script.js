28c14cdf8b7bcdf0ad1bbdba26eb86ad022450b6
Luciana <carrascocarhuasluciana0@gmail.com>
2024-09-30 20:23:30 -0500

    firebase configurado

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCS_piYj9xVuiSUglh19Oo4YaXgSFvVz_k",
+    authDomain: "diario-digital-72be8.firebaseapp.com",
+    projectId: "diario-digital-72be8",
+    storageBucket: "diario-digital-72be8.appspot.com",
+    messagingSenderId: "47542908941",
+    appId: "1:47542908941:web:9e9ba68b8bf9cf3562bd44"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
